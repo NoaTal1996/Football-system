f8ef3a242edb19c1f5e8b37800ebf6d36b993a9c
NoaTal1996 <noaai@post.bgu.ac.il>
2021-06-06 13:28:12 +0300

    tests_game

--- project/tests/units/scheduele_games.js
+++ project/tests/units/scheduele_games.js
@@ -1,25 +1,26 @@
 const app = require('../../main');
-const request = require('supertest')
+const request = require('supertest');
+const login = require('../units/login.test');
 jest.setTimeout(100000);
 
-async function addgame(gameday, gametime, hometeam, awayteam,field,game_Id,referee){
+async function addgame(gameday, gametime, Hometeam, Awayteam,Field,Referee){
     const res = await request(app)
         .post("/users/representive/addGame")
         .send({
-            GameDay:gameday,
-            GameTime:gametime,
-            HomeTeam:hometeam,
-            AwayTeam:awayteam,
-            Field:field,
-            Game_Id:game_Id,
-            Referee:referee
+            date:gameday,
+            time:gametime,
+            hometeam:Hometeam,
+            awayTeam:Awayteam,
+            field:Field,
+            referee:Referee
         })
         return res
 }
 
-describe('Referee register is created successfully', () => {
+describe('add game is created successfully', () => {
     test('should create a new post', async () => {
-        const res = await registerTest("AL", "Roi", "Reinshtein", "Israel", "shiba@inu3", "example@gmail.com", "https://res.cloudinary.com/db8c94xbz/image/upload/v1620751152/shiba_kufmdi.jpg", "judge");
+        const log = login.loginTest(admin,admin)
+        const res = await addgame("2022-05-22","20:00","Midtjylland","København","Parken","ElonMusk");
         expect(res.statusCode).toEqual(201);
     })
 })
