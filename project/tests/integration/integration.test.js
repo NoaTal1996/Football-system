644fe3ddb3dd5d5936c25853e25e1cf9ec7feec8
NoaTal1996 <noaai@post.bgu.ac.il>
2021-06-06 19:06:48 +0300

    :(

--- project/tests/integration/integration.test.js
+++ project/tests/integration/integration.test.js
@@ -8,9 +8,9 @@ jest.setTimeout(100000);
 
 describe('Register and Login is created successfully', () => {
     test('should create a new post', async () => {
-        const register = await regis.registerTest("roi16782223", "Roi", "Reinshtein", "Israel", "shiba@inu3", "example@gmail.com", "https://res.cloudinary.com/db8c94xbz/image/upload/v1620751152/shiba_kufmdi.jpg", "judge");
-        if(register.statusCode == 200){
-            const res = await log.loginTest("roi16782223","shiba@inu3");
+        const register = await regis.registerTest("roi84222340067", "Roi", "Reinshtein", "Israel", "shiba@inu3", "example@gmail.com", "https://res.cloudinary.com/db8c94xbz/image/upload/v1620751152/shiba_kufmdi.jpg", "judge");
+        if(register.statusCode === 201){
+            const res = await log.loginTest("roi84222340067","shiba@inu3");
             expect(res.statusCode).toEqual(200)
         }
     })
--- project/tests/units/referee_register.test.js
+++ project/tests/units/referee_register.test.js
@@ -21,8 +21,9 @@ async function registerTest(userName, Firstname, Lastname, Country, Password, Em
 
 // describe('Referee register is created successfully by representative', () => {
 //     test('should create a new post', async () => {
-//         const res = await registerTest("noa1234567", "Test", "Test", "Israel", "admin", "examddple@gmail.com", "https://res.cloudinary.com/db8c94xbz/image/upload/v1620751152/shiba_kufmdi.jpg", "judge");
-//         expect(res.statusCode).toEqual(201);
+//         const res = await registerTest("noa12304444444400056", "Test", "Test", "Israel", "shiba@inu3", "examddple@gmail.com", "https://res.cloudinary.com/db8c94xbz/image/upload/v1620751152/shiba_kufmdi.jpg", "judge");
+//         console.log(res)
+//         expect(res.status).toEqual(201);
 //     })
 // })
 
